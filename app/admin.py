737474a7af0e92fd8ac16f79d141025180c3ae8f
Code021-raelle <gabrielakinshola3@gmail.com>
2024-09-18 08:55:30 +0100

    updated the admin page to relate with all models available

--- app/admin.py
+++ app/admin.py
@@ -2,7 +2,7 @@ from flask import redirect, url_for
 from flask_admin import AdminIndexView, Admin
 from flask_admin.contrib.sqla import ModelView
 from flask_login import current_user
-from app.models import User, Listing, Review, Message, Booking, Chat
+from app.models import User, Listing, Review, Message, Booking, Chat, State, Amenity, ListingAmenity, Image
 from app import app, db
 
 class MyModelView(ModelView):
@@ -25,3 +25,8 @@ admin.add_view(MyModelView(Listing, db.session))
 admin.add_view(MyModelView(Review, db.session))
 admin.add_view(MyModelView(Message, db.session))
 admin.add_view(MyModelView(Booking, db.session))
+admin.add_view(MyModelView(Chat, db.session))
+admin.add_view(MyModelView(State, db.session))
+admin.add_view(MyModelView(Amenity, db.session))
+admin.add_view(MyModelView(ListingAmenity, db.session))
+admin.add_view(MyModelView(Image, db.session))
